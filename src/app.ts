b36491a5b9ac1a93d34c5f63777ae72720a09dd3
Michael Luo <michael.luo0801@gmail.com>
2019-04-14 10:39:37 +1000

    add automation test

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import * as bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
 import * as express from 'express';
 import * as expressWinston from 'express-winston';
 import * as mongoose from 'mongoose';
 import * as winston from 'winston';
 
 import { Index } from '../src/routes';
-import { OrderRoute } from '../src/routes/order';
 import * as errorHandler from '../src/utils/errorHandler';
 import { APIRoute } from './routes/api';
+import { OrderRoute } from './routes/order';
 import { UserRoute } from './routes/user';
+import { OrderAPILogger } from './utils/logger';
 
 class App {
   public app: express.Application;
@@ -16,9 +18,20 @@ class App {
   public userRoutes: UserRoute = new UserRoute();
   public apiRoutes: APIRoute = new APIRoute();
   public orderRoutes: OrderRoute = new OrderRoute();
-  public mongoUrl: string = 'mongodb://localhost/order-api';
+
+  public mongoUrl: string;
+  public mongoUser: string;
+  public mongoPass: string;
 
   constructor() {
+    const path = `${__dirname}/../.env.${process.env.NODE_ENV}`;
+    dotenv.config({ path: path });
+    this.mongoUrl = `mongodb://${process.env.MONGODB_URL_PORT}/${
+      process.env.MONGODB_DATABASE
+    }`;
+    this.mongoUser = `${process.env.MONGODB_USER}`;
+    this.mongoPass = `${process.env.MONGODB_PASS}`;
+
     this.app = express();
     this.app.use(bodyParser.json());
 
@@ -40,9 +53,41 @@ class App {
     this.app.use(errorHandler.errorHandler);
   }
   private mongoSetup(): void {
-    mongoose
-      .connect(this.mongoUrl, { useNewUrlParser: true, useCreateIndex: true })
-      .then(() => console.log('connected to mongodDB'));
+    let options: mongoose.ConnectionOptions;
+
+    if (process.env.NODE_ENV !== 'prod') {
+      options = {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      };
+    } else {
+      options = {
+        user: this.mongoUser,
+        pass: this.mongoPass,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        autoIndex: false, // Don't build indexes
+        autoReconnect: true,
+        reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+        reconnectInterval: 500, // Reconnect every 500ms
+        poolSize: 10, // Maintain up to 10 socket connections
+        // If not connected, return errors immediately rather than waiting for reconnect
+        bufferMaxEntries: 0,
+        connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+      };
+    }
+    mongoose.connect(this.mongoUrl, options).then(
+      () => {
+        OrderAPILogger.logger.info(`Connected to MongoDB ${this.mongoUrl}`);
+      },
+      err => {
+        OrderAPILogger.logger.error(
+          `Failed to connect to ${this.mongoUrl}: ${err}`
+        );
+      }
+    );
   }
 }
 
