d58ec20fb7fa338143c54d0d30a303439e450546
Michael Luo <michael.luo0801@gmail.com>
2019-04-14 15:41:42 +1000

    update server

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import * as https from 'https';
 
 import app from './app';
 
-const httpsOptions = {
-  key: fs.readFileSync('./config/key.pem'),
-  cert: fs.readFileSync('./config/cert.pem'),
-};
 const PORT = process.env.PORT || 8080;
+if (process.env.NODE_ENV !== 'production') {
+  const httpsOptions = {
+    key: fs.readFileSync('./config/key.pem'),
+    cert: fs.readFileSync('./config/cert.pem'),
+  };
 
-https.createServer(httpsOptions, app).listen(PORT, () => {
-  console.log(`Server is running in https://localhost:${PORT}`);
-});
+  https.createServer(httpsOptions, app).listen(PORT, () => {
+    console.log(`Server is running in https://localhost:${PORT}`);
+  });
+} else { app.listen(PORT); }
