761b580288302f3ee18a82c30a04b7119394ebb9
Michael Luo <michael.luo0801@gmail.com>
2019-04-15 11:36:54 +1000

    update setting

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -17,4 +17,4 @@ const downloadFile = (
     .download(options);
 };
 
-downloadFile('envvars_order-api', '.env.prod', '.env.prod');
+downloadFile('asia.artifacts.order-api-237605.appspot.com', '.env.prod', '.env.prod');
